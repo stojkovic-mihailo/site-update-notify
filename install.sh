f1a0975293aba3ae78a9f2932d5f19a550fc65cb
stojkovic-mihailo <mihailo.stojkovic1@gmail.com>
2022-03-04 20:58:50 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -4,12 +4,24 @@ reqs=("curl" "notify-send" "paplay")
 dir_base="./"
 install_command=""
 
+#OS installation commands dict
+declare -A os_ic
+os_ic["opensuse-tumbleweed"]="zypper -n in "
+os_ic["debian"]="apt install "
+
 function set_install_command(){
-    install_command="zypper -n in "
+    os_id=$(grep -i '^ID="' /etc/os-release | sed 's/"//g' | sed 's/ID=//')
+    echo "OS id $os_id got, checking compatibility..."
+    if [ -v os_ic['$os_id'] ]; then
+        install_command=os_ic['$os_id']
+        echo "OS compatible with current version of the script"
+        return 0
+    else
+        echo "Operating system with id $os_id is not compatible. Exiting..."
+        return 1
+    fi
+    
 }
-
-
-
 
 function install_reqs(){
     
@@ -22,10 +34,7 @@ function install_reqs(){
         else
             echo "$req already installed"
         fi
-
     done
-
-
 }
 
     
@@ -40,13 +49,18 @@ function main(){
         return
     fi
     #Installing requirements
-    echo "Installing required software..."
     set_install_command
+    local status=$?
+    if [ $status -ne "0" ]; then
+        return 1
+    fi
+    echo "Installing required software..."
     install_reqs
     
     echo "Making directory tree at $dir_base"
     mkdir -p "$dir_base/entries/.tmp" 
     echo "All done."
+    return 0
 }
 
 
